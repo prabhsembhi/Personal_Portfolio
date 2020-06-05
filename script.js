5efb44bc8599012aefcfcec8796766e7b1bdb8f2
prabhsembhi <prabhpreetsingh013@gmail.com>
2020-06-05 16:52:56 -0700

    Made the changes in the Nav bar and aligned a few items in it.

---
+++ script.js
@@ -0,0 +1,22 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  var mediaQuery = window.matchMedia("(min-width: 1230px)");
+  if (mediaQuery.matches) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("nav").style.top = "0";
+    } else {
+      document.getElementById("nav").style.top = "-60px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+};
+
+
+$(".menu-btn").click(function(){
+    $(".menu-btn").toggleClass("close");
+    $("#nav").slideToggle();
+  });
+  $("#nav")
